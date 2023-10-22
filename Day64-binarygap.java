e22b4b729546eb58f519de8dd2a325e758617f03
Swetha PS <117517538+SwethaPS-05@users.noreply.github.com>
2023-10-22 17:09:14 +0530

    Update Day64-binarygap.java

--- Day64-binarygap.java
+++ Day64-binarygap.java
@@ -1,13 +1,17 @@
-public class Solution {
-    public int BinaryGap(int n) {
-        int last = -1, ans = 0;
-        for (int i = 0; i < 32; ++i)
-            if (((n >> i) & 1) > 0) {
-                if (last >= 0)
-                    ans = Math.Max(ans, i - last);
-                last = i;
+class Solution {
+    public int binaryGap(int n) {
+        String bin = Integer.toBinaryString(n);
+        int id = 0, max = -1;
+        for (int i = 0; i < bin.length(); i++) {
+            char c = bin.charAt(i);
+            if (c == '1') {
+                int cur = i - id;
+                if (max < cur) {
+                    max = cur;
+                }
+                id = i;
             }
-
-        return ans; 
+        }
+        return max;
     }
 }
